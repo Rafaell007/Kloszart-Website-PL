bf76c0c8aa3050cc391190feb81487485d1afd84
Rafał Kazberuk <drax1995@tlen.pl>
2025-10-17 18:27:43 +0200

    ended frame for modal

---
+++ scripts/modal.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('#calendare-btn');
+const modalBtn = document.querySelector('.open-calendar-btn');
+const modalExit = document.querySelector('.modal-exit');
+
+modalBtn.addEventListener('click', ()=>{
+    modal.classList.add("show-modal");
+})
+
+modalExit.addEventListener('click', ()=>{
+    modal.classList.remove('show-modal');
+})
