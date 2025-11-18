21e3d7927dca30f720338de0e12fa967e1114c79
Rafaell007 <drax1995@tlen.pl>
2025-11-18 01:21:21 +0100

    added navigation jumps

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -21,4 +21,25 @@ function textAnimation () {
 
 window.addEventListener("scroll", textAnimation);
 
+
+const navLinks = document.querySelectorAll('a[href^="#"]');
+
+navLinks.forEach((link)=>{
+    link.addEventListener("click", (e)=>{
+        e.preventDefault();
+
+        const href = link.getAttribute("href");
+        const sectionId = href.substring(1);
+        const targetSection = document.getElementById(sectionId);
+
+        if(targetSection){
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: "start"
+            });
+        }
+
+    });
+});
+
 modal();
